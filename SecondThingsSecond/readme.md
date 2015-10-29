232c419fbe399c6a39a29bf6cf43cdbcf73f53b1
EricDeiman <deiman.eric@gmail.com>
2015-10-28 19:58:08 -0500

    Started adding code to play the game.

--- SecondThingsSecond/readme.md
+++ SecondThingsSecond/readme.md
@@ -2,4 +2,4 @@ I don't have time to write much now, but I'll update this space as I find time.
 
 In the mean time, you might recognize the file in the source folder as the file from the FistThingsFirst folder.  We're going to be expanding it in this lesson.  In particular, we're going to be adding a game.
 
-[Does putting a link to the wiki here allow access to the wiki from a mobile device?](../../../wiki/SecondThingsSecond)
+[Click here to find more information on the wiki page for this lesson.](../../../wiki/SecondThingsSecond)
--- SecondThingsSecond/src/HelloWorld.java
+++ SecondThingsSecond/src/HelloWorld.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Random;
 
 /*
     The traditional second program.
@@ -21,28 +22,71 @@ import java.io.IOException;
 
 /**
  * It's tradition that the second program a person writes in a new language
- * plays a game with the user on the console.  That's what we'll expand this 
+ * plays a game with the user on the console. That's what we'll expand this
  * class to do.
  *
  * @author eric
  *
  */
 public class HelloWorld {
-	
+
 	/**
-	 * This may still look like gibberish, but it marks what is supposed to happen
-	 * when the program starts.  Don't worry too much about what it means; we'll
-	 * get to that in due course.
+	 * This may still look like gibberish, but it marks what is supposed to
+	 * happen when the program starts. Don't worry too much about what it means;
+	 * we'll get to that in due course.
 	 * 
-	 * @param args The command line parameters.
-	 * @throws IOException 
+	 * @param args
+	 *            The command line parameters.
+	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		System.out.println("Hello world!");
-		byte[] buffer = new byte[255];
-		System.out.print("What's your name? ");
-		System.in.read(buffer);
-		String name = new String(buffer).trim();
-		System.out.println("Hello there, " + name + "!");
+	public static void main( String[] args ) throws IOException {
+		System.out.println( "Hello world!" );
+
+		// Putting empty line around portions of your code can make it easier
+		// for a human reader to see what's going on. So, grouping lines of code
+		// that work together is a good idea.
+		byte[] buffer = new byte[ 255 ];
+		System.out.print( "What's your name? " );
+		System.in.read( buffer );
+		String name = new String( buffer ).trim();
+		System.out.println( "Hello there, " + name + "!" );
+
+		// The above code all works together to read a persons name and display
+		// it on the console, so it has empty line before the group and after
+		// the group. The lines below are all related to playing the game.
+		System.out.print( "Shall we play a game? " );
+		System.in.read( buffer );
+		if ( buffer[ 0 ] == 'Y' || buffer[ 0 ] == 'y' ) {
+			// I put the code to play the game in what's called a method. You
+			// can find it lower down in this file. Look for the phrase
+			// "playGame" to
+			// find it.
+			playGame();
+		}
+	}
+
+	/**
+	 * This is called a method. In Java, when you have several lines of code
+	 * that work together and that do something that you'll want to do more than
+	 * once (or that deserve their own name), you make a method out of it.
+	 * 
+	 * This is a computerized version of the number guessing game. A pair of
+	 * younger kids play a where one picks a number and the other tries to guess
+	 * it. In this version of the game, the computer will guess a number and the
+	 * user tries to guess it.
+	 * 
+	 * @throws IOException
+	 */
+	public static void playGame() throws IOException {
+		// What is the biggest number the computer might pick?
+		int upperBound = 10;
+		// Computers can do things that are (kind of) random.
+		Random randomNumberMaker = new Random();
+		int guess = randomNumberMaker.nextInt( upperBound );
+
+		// This is weird, so I wrote more about it in the wiki page for this
+		// lesson.
+		guess = guess + 1;
+		
 	}
 }
